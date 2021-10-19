cc2e4a113124741809479658c40cfccb9779b78d
Diego <diegoserranor@gmail.com>
2021-10-19 12:55:46 -0500

    Add svg icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Svg, Rect, Path} from 'react-native-svg';
+
+interface IconProps {
+  color?: string;
+  size?: number;
+}
+
+export function AddTaskIcon({color, size}: IconProps) {
+  return (
+    <Svg
+      fill={color}
+      width={size}
+      height={size}
+      enable-background="new 0 0 24 24"
+      viewBox="0 0 24 24">
+      <Rect fill="none" height="24" width="24" />
+      <Path d="M22,5.18L10.59,16.6l-4.24-4.24l1.41-1.41l2.83,2.83l10-10L22,5.18z M12,20c-4.41,0-8-3.59-8-8s3.59-8,8-8 c1.57,0,3.04,0.46,4.28,1.25l1.45-1.45C16.1,2.67,14.13,2,12,2C6.48,2,2,6.48,2,12s4.48,10,10,10c1.73,0,3.36-0.44,4.78-1.22 l-1.5-1.5C14.28,19.74,13.17,20,12,20z M19,15h-3v2h3v3h2v-3h3v-2h-3v-3h-2V15z" />
+    </Svg>
+  );
+}
+
+export function LightModeIcon({color, size}: IconProps) {
+  return (
+    <Svg
+      fill={color}
+      width={size}
+      height={size}
+      enable-background="new 0 0 24 24"
+      viewBox="0 0 24 24">
+      <Rect fill="none" height="24" width="24" />
+      <Path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5 S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1 s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0 c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95 c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41 L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41 s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06 c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z" />
+    </Svg>
+  );
+}
+
+export function DarkModeIcon({color, size}: IconProps) {
+  return (
+    <Svg
+      fill={color}
+      width={size}
+      height={size}
+      enable-background="new 0 0 24 24"
+      viewBox="0 0 24 24">
+      <Rect fill="none" height="24" width="24" />
+      <Path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z" />
+    </Svg>
+  );
+}
+
+export function DeleteIcon({color, size}: IconProps) {
+  return (
+    <Svg fill={color} width={size} height={size} viewBox="0 0 24 24">
+      <Path d="M0 0h24v24H0V0z" fill="none" />
+      <Path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
+    </Svg>
+  );
+}
+
+export function SaveIcon({color, size}: IconProps) {
+  return (
+    <Svg fill={color} width={size} height={size} viewBox="0 0 24 24">
+      <Path d="M0 0h24v24H0V0z" fill="none" />
+      <Path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm2 16H5V5h11.17L19 7.83V19zm-7-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zM6 6h9v4H6z" />
+    </Svg>
+  );
+}
